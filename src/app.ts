7f440e22f5c91c5535b24d29e6d31f9ea8f3826f
yanislammari <yanis.lammari937@gmail.com>
2025-03-22 00:12:17 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { middleware } from "./config/middlewares";
+
+dotenv.config();
+const IP_ADRESS = process.env.IP_ADRESS as string;
 
 const app = express();
+
+app.use(cors({
+  origin: IP_ADRESS,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+middleware(app);
 
 export default app;

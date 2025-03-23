daf545ab8c35bc8cf8d09517f6445f8ea78b4747
yanislammari <yanis.lammari937@gmail.com>
2025-03-23 21:20:57 +0100

    fix somes schemas

--- src/schemas/request/user.request.schema.ts
+++ src/schemas/request/user.request.schema.ts
@@ -4,8 +4,8 @@ import Role from "../../models/enums/role";
 
 const UserRequestSchema = joi.object<User>({
   email: joi.string().email().required(),
-  firstName: joi.string().required(),
-  lastName: joi.string().required(),
+  firstName: joi.string().min(3).max(100).required(),
+  lastName: joi.string().min(3).max(100).required(),
   rating: joi.number().min(0).max(5).required(),
   password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).required(),
   role: joi.string().valid(...Object.values(Role)).default(Role.User).optional()

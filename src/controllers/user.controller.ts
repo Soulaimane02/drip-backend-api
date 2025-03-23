0b551f377a153adc743a06ad9321dafa5b324516
yanislammari <yanis.lammari937@gmail.com>
2025-03-23 17:35:37 +0100

    add delete user request

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -59,6 +59,23 @@ class UserController {
       return res.status(500).json({ error: "Internal server error" });
     }
   }
+
+  async deleteUser(req: Request, res: Response) { 
+    try {
+      const id = req.params.id;
+      await this.userService.deleteUser(id);
+      return res.status(200).json({ message: "User deleted successfully" });
+    }
+    catch(err: any) {
+      if(err.message === "User not found !") {
+        return res.status(404).json({ error: "User not found" });
+      }
+      else if(err.name === "CastError") {
+        return res.status(400).json({ error: "Invalid ID format" });
+      }
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
 }
 
 export default UserController;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,5 +7,6 @@ const userController = new UserController();
 router.get("/", userController.getAllUsers.bind(userController));
 router.get("/:id", userController.getUserById.bind(userController));
 router.put("/:id", userController.updateUser.bind(userController));
+router.delete("/:id", userController.deleteUser.bind(userController));
 
 export default router;

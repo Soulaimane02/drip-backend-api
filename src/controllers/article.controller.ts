7e1d351fc44305d8bea67e0b20169f790283069e
yanislammari <yanis.lammari937@gmail.com>
2025-03-25 20:17:42 +0100

    add get category trees of article request

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import ArticleService from "../services/article.service";
 import ArticleRequestSchema from "../schemas/request/article.request.schema";
 import ArticleRequestDTO from "../models/entities/article/dto/article.request.dto";
+import CategoryService from "../services/category.service";
 
 class ArticleController {
   private readonly articleService: ArticleService;
+  private readonly categoryService: CategoryService;
 
   constructor() {
     this.articleService = new ArticleService();
+    this.categoryService = new CategoryService();
   }
 
   async getAllArticles(req: Request, res: Response) {
@@ -25,6 +28,30 @@ class ArticleController {
       const id = req.params.id;
       const article = await this.articleService.getArticleById(id);
       return res.status(200).json(article);
+    }
+    catch(err: any) {
+      if(err.message === "Article not found !") {
+        return res.status(404).json({ error: "Article not found" });
+      }
+      else if(err.name === "CastError") {
+        return res.status(400).json({ error: "Invalid ID format" });
+      }
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  async getCategoryTreesOfArticle(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const article = await this.articleService.getArticleById(id);
+      let trees: string[] = [];
+
+      article.categories.forEach(async (category) => {
+        const tree = await this.categoryService.getCategoryTree(category);
+        trees.push(tree);
+      });
+
+      return res.status(200).json({ trees: trees });
     }
     catch(err: any) {
       if(err.message === "Article not found !") {

4c9f4b655bb93588b9d30de07f3f722b9cdfe816
yanislammari <yanis.lammari937@gmail.com>
2025-03-26 16:24:39 +0100

    add payment repository

---
+++ src/repositories/payment.repository.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import Repository from "../config/repository";
+import Payment from "../models/entities/payment/payment";
+import PaymentDatabaseSchema from "../schemas/database/payment.database.schema";
+
+class PaymentRepository implements Repository<Payment> {
+  private readonly paymentModel;
+
+  constructor() {
+    this.paymentModel = mongoose.model<Payment>("Payment", PaymentDatabaseSchema);
+  }
+
+  async getAll(): Promise<Payment[]> {
+    const payments = await this.paymentModel.find();
+    return payments.map((payment) => payment.toObject());
+  }
+  
+  async get(id: String): Promise<Payment> {
+    const payment = await this.paymentModel.findById(id);
+    if(!payment) {
+      throw new Error("Payment not found !");
+    }
+    return payment?.toObject();
+  }
+
+  async add(paymentToAdd: Payment): Promise<Payment> {
+    const payment = await new this.paymentModel(paymentToAdd).save();
+    return payment.toObject();
+  }
+  
+  async put(id: String, newPayment: Payment): Promise<Payment> {
+    const payment = await this.paymentModel.findByIdAndUpdate(id, newPayment, {new: true});
+    if(!payment) {
+      throw new Error("Payment not found !");
+    }
+    return payment?.toObject();
+  }
+  
+  async delete(id: String): Promise<void> {
+    const payment = await this.paymentModel.findByIdAndDelete(id);
+    if(!payment) {
+      throw new Error("Payment not found !");
+    }
+  }
+}
+
+export default PaymentRepository;

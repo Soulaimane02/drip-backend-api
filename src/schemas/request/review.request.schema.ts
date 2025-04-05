50ce3e1bda84887c35dffc65aab19ab887ffa8f9
yanislammari <yanis.lammari937@gmail.com>
2025-04-05 15:44:48 +0200

    add review schemas

---
+++ src/schemas/request/review.request.schema.ts
@@ -0,0 +1,11 @@
+import joi from "joi";
+import Review from "../../models/entities/review/review";
+
+const ReviewRequestSchema = joi.object<Review>({
+  rating: joi.number().min(0.5).max(5).required(),
+  comment: joi.string().min(3).max(1000).required(),
+  userId: joi.string().hex().length(24).required(),
+  articleId: joi.string().hex().length(24).required()
+});
+
+export default ReviewRequestSchema;

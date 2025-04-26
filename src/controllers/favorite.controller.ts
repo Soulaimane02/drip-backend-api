0adb17518edc5e89a03bb3ebc771a9bf50e2dbeb
yanislammari <yanis.lammari937@gmail.com>
2025-04-26 21:21:00 +0200

    add add favorite request

--- src/controllers/favorite.controller.ts
+++ src/controllers/favorite.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import FavoriteService from "../services/favorite.service";
+import FavoriteRequestSchema from "../schemas/request/favorite.request.schema";
+import FavoriteRequestDTO from "../models/entities/favorite/dto/favorite.request.dto";
 
 class FavoriteController {
   private readonly favoriteService: FavoriteService;
@@ -23,6 +25,22 @@ class FavoriteController {
       const id = req.params.id;
       const favorite = await this.favoriteService.getByUserId(id);
       return res.status(200).json(favorite);
+    }
+    catch(err) {
+      next(err);
+    }
+  }
+
+  async addFavorite(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { error, value } = FavoriteRequestSchema.validate(req.body);
+      if(error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const favorite: FavoriteRequestDTO = value;
+      const addedFavorite = await this.favoriteService.addFavorite(favorite);
+      return res.status(201).json(addedFavorite);
     }
     catch(err) {
       next(err);
--- src/routes/favorite.routes.ts
+++ src/routes/favorite.routes.ts
@@ -7,6 +7,7 @@ const favoriteRoutes = () => {
 
   router.get("/", favoriteController.getAllFavorites.bind(favoriteController));
   router.get("/user/:id", favoriteController.getFavoriteByUserId.bind(favoriteController));
+  router.post("/", favoriteController.addFavorite.bind(favoriteController));
 
   return router;
 }

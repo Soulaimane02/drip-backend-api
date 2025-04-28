d5d7299041716b6c4978044f99b80d5c6cbed00b
yanislammari <yanis.lammari937@gmail.com>
2025-04-28 20:06:35 +0200

    add verify favorite ownership middleware

--- src/routes/favorite.routes.ts
+++ src/routes/favorite.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import FavoriteController from "../controllers/favorite.controller";
 import { authorisationMiddleware, isSignedInMiddleware } from "../middlewares/base.middlewares";
 import Role from "../models/enums/role";
+import { verifyFavoriteOwnershipMiddleware } from "../middlewares/favorite.middlewares";
 
 const favoriteRoutes = () => {
   const router = express.Router();
@@ -10,7 +11,7 @@ const favoriteRoutes = () => {
   router.get("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin]), favoriteController.getAllFavorites.bind(favoriteController));
   router.get("/user/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), favoriteController.getFavoritesByUserId.bind(favoriteController));
   router.get("/article/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller]), favoriteController.getFavoritesByArticleId.bind(favoriteController));
-  router.post("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), favoriteController.addFavorite.bind(favoriteController));
+  router.post("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyFavoriteOwnershipMiddleware(), favoriteController.addFavorite.bind(favoriteController));
   router.delete("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), favoriteController.deleteFavorite.bind(favoriteController));
 
   return router;

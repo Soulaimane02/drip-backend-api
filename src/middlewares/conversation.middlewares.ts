d50b26a0d732885d5e88f04351637826404ec430
yanislammari <yanis.lammari937@gmail.com>
2025-04-29 01:57:42 +0200

    add verify conversation ownership middleware

---
+++ src/middlewares/conversation.middlewares.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { getUserByToken } from "../utils/token";
+import ConversationRepository from "../repositories/conversation.repository";
+
+const conversationRepository = new ConversationRepository(); 
+
+export const verifyConversationOwnershipMiddleware = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1];
+      const user = await getUserByToken(token as string);
+      const conversationId = req.params.id;
+      const conversation = await conversationRepository.get(conversationId);
+
+      if(conversation.firstUserId !== user.id && conversation.secondUserId !== user.id) {
+        return res.status(403).json({ error: "You do not have permission in this conversation" });
+      }
+
+      next();
+    }
+    catch(err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+};
--- src/routes/conversation.routes.ts
+++ src/routes/conversation.routes.ts
@@ -2,16 +2,17 @@ import express from "express";
 import ConversationController from "../controllers/conversation.controller";
 import { authorisationMiddleware, isSignedInMiddleware } from "../middlewares/base.middlewares";
 import Role from "../models/enums/role";
+import { verifyConversationOwnershipMiddleware } from "../middlewares/conversation.middlewares";
 
 const conversationRoutes = () => {
   const router = express.Router();
   const conversationController = new ConversationController();
 
-  router.get("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), conversationController.getAllConversations.bind(conversationController));
-  router.get("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), conversationController.getConversationById.bind(conversationController));
+  router.get("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin]), conversationController.getAllConversations.bind(conversationController));
+  router.get("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyConversationOwnershipMiddleware(), conversationController.getConversationById.bind(conversationController));
   router.get("/user/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), conversationController.getConversationsByUserId.bind(conversationController));
   router.post("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), conversationController.startConversation.bind(conversationController));
-  router.delete("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), conversationController.deleteConversation.bind(conversationController));
+  router.delete("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyConversationOwnershipMiddleware(), conversationController.deleteConversation.bind(conversationController));
 
   return router;
 };

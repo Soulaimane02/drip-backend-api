5abbde6b1f7bff0135ead6f731e5774a952b6e35
yanislammari <yanis.lammari937@gmail.com>
2025-04-30 02:25:44 +0200

    add verify message ownership by conversation id middleware

--- src/middlewares/message.middlewares.ts
+++ src/middlewares/message.middlewares.ts
@@ -46,3 +46,23 @@ export const verifyMessageOwnershipCriticalMiddleware = () => {
     }
   };
 };
+
+export const verifyMessageOwnershipByConversationIdMiddleware = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1];
+      const user = await getUserByToken(token as string);
+      const conversationId = req.params.id;
+      const conversation = await conversationRepository.get(conversationId);
+
+      if(conversation.firstUserId !== user.id && conversation.secondUserId !== user.id) {
+        return res.status(403).json({ error: "You do not have permission in this conversation" });
+      }
+
+      next();
+    }
+    catch(err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+};
--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -3,7 +3,7 @@ import MessageController from "../controllers/message.controller";
 import { Server } from "socket.io";
 import { authorisationMiddleware, isSignedInMiddleware } from "../middlewares/base.middlewares";
 import Role from "../models/enums/role";
-import { verifyMessageOwnershipCriticalMiddleware, verifyMessageOwnershipMiddleware } from "../middlewares/message.middlewares";
+import { verifyMessageOwnershipByConversationIdMiddleware, verifyMessageOwnershipCriticalMiddleware, verifyMessageOwnershipMiddleware } from "../middlewares/message.middlewares";
 
 const messageRoutes = (io: Server) => {
   const router = express.Router();
@@ -11,7 +11,7 @@ const messageRoutes = (io: Server) => {
 
   router.get("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin]), messageController.getAllMessages.bind(messageController));
   router.get("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyMessageOwnershipMiddleware(), messageController.getMessageById.bind(messageController));
-  router.get("/conversation/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), messageController.getMessagesByConversationId.bind(messageController));
+  router.get("/conversation/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyMessageOwnershipByConversationIdMiddleware(), messageController.getMessagesByConversationId.bind(messageController));
   router.post("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), messageController.sendMessage.bind(messageController));
   router.put("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyMessageOwnershipCriticalMiddleware(), messageController.updateMessage.bind(messageController));
   router.put("/respond/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), messageController.respondOffer.bind(messageController));

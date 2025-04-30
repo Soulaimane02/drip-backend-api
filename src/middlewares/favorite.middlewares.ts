d1ebe5a790d13180a31592b402f9654d6da9a6bb
yanislammari <yanis.lammari937@gmail.com>
2025-04-30 02:09:36 +0200

    add verify favorite ownership by article id middleware

--- src/middlewares/favorite.middlewares.ts
+++ src/middlewares/favorite.middlewares.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { getUserByToken } from "../utils/token";
 import FavoriteRepository from "../repositories/favorite.repository";
+import ArticleRepository from "../repositories/article.repository";
 
 const favoriteRepository = new FavoriteRepository();
+const articleRepository = new ArticleRepository();
 
 export const verifyFavoriteOwnershipMiddleware = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -33,6 +35,26 @@ export const verifyFavoriteOwnershipByFavoriteIdMiddleware = () => {
 
       if(favorite.userId !== user.id) {
         return res.status(403).json({ error: "You do not have permission to delete this favorite" });
+      }
+
+      next();
+    }
+    catch(err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+};
+
+export const verifyFavoriteOwnershipByArticleIdMiddleware = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1];
+      const user = await getUserByToken(token as string);
+      const articleId = req.params.id;
+      const article = await articleRepository.get(articleId);
+
+      if(article.userId !== user.id) {
+        return res.status(403).json({ error: "You do not have permission to get favorites for this article" });
       }
 
       next();
--- src/routes/favorite.routes.ts
+++ src/routes/favorite.routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import FavoriteController from "../controllers/favorite.controller";
 import { authorisationMiddleware, isSignedInMiddleware } from "../middlewares/base.middlewares";
 import Role from "../models/enums/role";
-import { verifyFavoriteOwnershipByFavoriteIdMiddleware, verifyFavoriteOwnershipMiddleware } from "../middlewares/favorite.middlewares";
+import { verifyFavoriteOwnershipByArticleIdMiddleware, verifyFavoriteOwnershipByFavoriteIdMiddleware, verifyFavoriteOwnershipMiddleware } from "../middlewares/favorite.middlewares";
 
 const favoriteRoutes = () => {
   const router = express.Router();
@@ -10,7 +10,7 @@ const favoriteRoutes = () => {
 
   router.get("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin]), favoriteController.getAllFavorites.bind(favoriteController));
   router.get("/user/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), favoriteController.getFavoritesByUserId.bind(favoriteController));
-  router.get("/article/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller]), favoriteController.getFavoritesByArticleId.bind(favoriteController));
+  router.get("/article/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller]), verifyFavoriteOwnershipByArticleIdMiddleware(), favoriteController.getFavoritesByArticleId.bind(favoriteController));
   router.post("/", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyFavoriteOwnershipMiddleware(), favoriteController.addFavorite.bind(favoriteController));
   router.delete("/:id", isSignedInMiddleware(), authorisationMiddleware([Role.Admin, Role.Seller, Role.User]), verifyFavoriteOwnershipByFavoriteIdMiddleware(), favoriteController.deleteFavorite.bind(favoriteController));
 

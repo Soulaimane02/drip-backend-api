def8db46ccb275b7918a5cecf97ea600f998a13a
yanislammari <yanis.lammari937@gmail.com>
2025-04-30 02:29:25 +0200

    add verify message sender ownership middleware

--- src/middlewares/message.middlewares.ts
+++ src/middlewares/message.middlewares.ts
@@ -66,3 +66,22 @@ export const verifyMessageOwnershipByConversationIdMiddleware = () => {
     }
   };
 };
+
+export const verifyMessageSenderOwnershipMiddleware = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1];
+      const user = await getUserByToken(token as string);
+      const userId = req.body.userId;
+
+      if(userId !== user.id) {
+        return res.status(403).json({ error: "You do not have permission to send this message" });
+      }
+
+      next();
+    }
+    catch(err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+};

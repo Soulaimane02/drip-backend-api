2d6edb84a392ccc417d467eee2d90ebd44e4c23f
yanislammari <yanis.lammari937@gmail.com>
2025-05-02 16:25:20 +0200

    fix server listen

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import server from "./src/app";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Running : ${PORT}`);
 });

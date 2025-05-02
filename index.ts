25303d9022ca7eb93e6f5f6a00312630e89b3497
yanislammari <yanis.lammari937@gmail.com>
2025-05-02 16:31:37 +0200

    fix environement docker

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import server from "./src/app";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = parseInt(process.env.PORT || "3000", 10);
+const PORT = parseInt(process.env.PORT as string, 10);
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Running : ${PORT}`);
